aa4e967af4686d27b44a35f98842633959783e07
Fernando Jascovich <fernando.ej@gmail.com>
2016-07-26 19:10:25 -0300

    updates and animations

--- modules/AdClient.js
+++ modules/AdClient.js
@@ -4,28 +4,49 @@ import {
   Text,
   View,
   WebView,
-  Dimensions
+  Dimensions,
+  Animated
 } from 'react-native';
 
 class AdClient extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            loading: true,
             html: "",
             h: 0,
-            w: 0
+            w: 0,
+            layoutHeight: new Animated.Value(0)
         };
+        this.lastRequest = "";
+        this.mounted = false;
         this.getAd();
     }
+    componentDidMount() {
+        this.mounted = true;
+    }
+    componentWillUnmount() {
+        this.mounted = false;
+    }
     getAd() {
+        if(this.mounted) {
+            this.setState({
+                html: "",
+                h: 0,
+                w: 0,
+                loading: true
+            });
+        }
         let dim = Dimensions.get('window');
         h = (Math.random() > 0.5) ? 50 : 100;
         w = (Math.random() > 0.5) ? 300 : 320;
         let placement = "9243149";
-        let url = "http://mobile.adnxs.com/ssmob?id=" + 
-            placement + 
-            "&size=" + w + "x" + h + 
+        //let placement = "1281432";
+        let url = "https://mobile.adnxs.com/ssmob?id=" +
+            placement +
+            "&size=" + w + "x" + h +
             "&format=json";
+        this.lastRequest = url;
         fetch(url)
             .then((response) => response.json())
             .then((responseJson) => {
@@ -35,36 +56,73 @@ class AdClient extends React.Component {
                 html += '</style></head><body>';
                 html += responseJson.ads[0].content;
                 html += "</body></html>";
-                this.setState({ 
+                this.setState({
                     html: html,
+                    loading: false,
                     h: responseJson.ads[0].height,
-                    w: responseJson.ads[0].width
+                    w: responseJson.ads[0].width,
+                    layoutHeight: new Animated.Value(0)
                 });
             })
-            .catch((error) => { });
+            .catch((error) => {
+                console.log(error);
+                this.setState({
+                    html: "",
+                    h: 0,
+                    w: 0,
+                    loading: false
+                });
+            });
         }
+      getLoadingView() {
+            return (<View style={styles.adLayoutContainer}>
+                        <View style={styles.adLayout}>
+                            <Text>Requesting ad server...</Text>
+                        </View>
+                    </View>);
+      }
+      getErrorView() {
+            return (<View style={styles.adLayoutContainer}>
+                        <View style={styles.adLayout}>
+                            <Text>
+                                Sorry, no ad here ({this.lastRequest}).
+                            </Text>
+                        </View>
+                    </View>);
+      }
       render() {
-          if(this.state.h < 2) {
-            return (<View />);
+          if(this.state.loading) {
+              return this.getLoadingView();
+          } else if(this.state.h < 2) {
+              return this.getErrorView();
+          } else {
+            Animated.timing(
+              this.state.layoutHeight,
+              {
+                  velocity: 2,
+                  deceleration: 0.5,
+                  toValue: this.state.h
+              }
+            ).start();
+            return (
+            <View style={styles.adLayoutContainer}>
+                <View style={styles.adLayout}>
+                    <Text style={styles.adLabel}>Publicidad</Text>
+                    <Animated.View style={{
+                        height: this.state.layoutHeight,
+                        width: this.state.w,
+                        alignSelf: 'center'
+                        }}>
+                    <WebView
+                        automaticallyAdjustContentInsets={false}
+                        scalesPageToFit={true}
+                        scrollEnabled={false}
+                        source={{html:this.state.html}} />
+                    </Animated.View>
+                </View>
+            </View>
+            );
           }
-        return (
-        <View style={styles.adLayoutContainer}>
-            <View style={styles.adLayout}>
-                <Text style={styles.adLabel}>Publicidad</Text>
-                <View style={{
-                    height: this.state.h,
-                    width: this.state.w,
-                    alignSelf: 'center'
-                    }}>
-                <WebView
-                    automaticallyAdjustContentInsets={false}
-                    scalesPageToFit={true}
-                    scrollEnabled={false}
-                    source={{html:this.state.html}} />
-            </View>
-            </View>
-        </View>
-        );
     }
 }
 
@@ -74,7 +132,18 @@ const styles = StyleSheet.create({
     adLayout: {
         backgroundColor: '#ffffff',
         flexDirection: 'column',
-        alignItems: 'center'
+        alignItems: 'center',
+        borderTopLeftRadius: 4,
+        borderTopRightRadius: 4,
+        elevation: 7,
+        marginTop: 7,
+        shadowColor: "#000000",
+        shadowOpacity: 0.8,
+        shadowRadius: 3,
+        shadowOffset: {
+          height: 0,
+          width: 0
+        }
     },
     adLabel: {
         textAlign: 'left',
@@ -83,7 +152,7 @@ const styles = StyleSheet.create({
         marginBottom: 4,
         marginLeft: 10,
         fontSize: 10,
-        elevation: 10
+        alignSelf: 'flex-start'
     }
 });
 
